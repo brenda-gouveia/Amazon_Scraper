f1d7430a39dd0744cd76eaf01fc7ca798c9ab8c4
brendagouveia <brendagouveia@id.uff.br>
2025-03-26 23:34:08 -0300

    add static file serving and style for the Amazon Scraper

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ import { JSDOM } from "jsdom";
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
   });
-  
+
 app.get("/api/scrape", async (req, res) => {
     const keyword = req.query.keyword;
     if (!keyword) {
