ec372a94106811bf12bf4e194c27289bc19ecfec
brendagouveia <brendagouveia@id.uff.br>
2025-03-26 22:41:06 -0300

    add homepage route and initial HTML structure for Amazon Scraper

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ import { JSDOM } from "jsdom";
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+  });
+  
 app.get("/api/scrape", async (req, res) => {
     const keyword = req.query.keyword;
     if (!keyword) {
         return res.status(400).json({ error: "Keyword is required" });
     }
     console.log(`Scraping for keyword: ${keyword}`);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
